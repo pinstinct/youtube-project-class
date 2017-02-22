4b1382781b0d3e62473f1889d4487d37f7fb8e03
LimHM <gusals3407@gmail.com>
2017-02-22 21:55:40 +0900

    index page 추가, api 키 가져오는 함수 구현

--- django_app/youtube/settings.py
+++ django_app/youtube/settings.py
@@ -9,16 +9,12 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-import json
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-CONF_DIR = os.path.join(ROOT_DIR, '.conf')
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
-content = json.loads(open(os.path.join(CONF_DIR, 'settings_local.json')).read())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
