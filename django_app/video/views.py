8eb5fd3acf729238f4deb8f4552d35f54a5ac249
LimHM <gusals3407@gmail.com>
2017-02-23 00:49:12 +0900

    이전, 다음 페이지 이동 기능 추가

--- django_app/video/views.py
+++ django_app/video/views.py
@@ -1,36 +1,44 @@
-import json
-
 import requests
 from dateutil.parser import parse
 from django.shortcuts import render
 
 from utils.settings import get_setting
-from .models import Video
 
 
-def get_search_list_from_youtube(keyword):
+def search_from_youtube(keyword, page_token=None):
     youtube_api_key = get_setting()['youtube']['API_KEY']
     payload = {
         'part': 'snippet',
         'q': keyword,
         'type': 'video',
-        'maxResults': 30,
+        'maxResults': 5,
         'key': youtube_api_key,
     }
+    if page_token:
+        payload['pageToken'] = page_token
     r = requests.get('https://www.googleapis.com/youtube/v3/search', params=payload)
-
     result_dic = r.json()
-    items = result_dic['items']
 
-    return items
+    return result_dic
 
 
 def search(request):
     videos = []
+    context = {
+        'videos': videos,
+    }
     keyword = request.GET.get('keyword', '').strip()
+    page_token = request.GET.get('page_token')
     if keyword != '':
-        item_dict = get_search_list_from_youtube(keyword)
-        for item in item_dict:
+        result_dic = search_from_youtube(keyword, page_token)
+        prev_page = result_dic.get('prevPageToken')
+        next_page = result_dic.get('nextPageToken')
+        context['prev_page'] = prev_page
+        context['next_page'] = next_page
+        context['keyword'] = keyword
+
+        items = result_dic['items']
+        for item in items:
             youtube_id = item['id']['videoId']
             thumbnail = item['snippet']['thumbnails']['high']['url']
             title = item['snippet']['title']
@@ -46,7 +54,4 @@ def search(request):
                 'published_date': published_date,
             }
             videos.append(item_dict)
-    context = {
-        'videos': videos,
-    }
     return render(request, 'video/search.html', context)
